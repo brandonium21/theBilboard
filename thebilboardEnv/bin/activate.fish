d4af803e3e42c4bf1d91b6adc6ddd0df3f2ab1cc
dontae1 <brandonium21@gmail.com>
2015-08-12 18:02:35 -0500

    3d commit

--- thebilboardEnv/bin/activate.fish
+++ thebilboardEnv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Brandonium21/Documents/projects/uberbilboard/uberbilboardEnv"
+set -gx VIRTUAL_ENV "/Users/Brandonium21/Documents/projects/TheBilboard/thebilboardEnv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
